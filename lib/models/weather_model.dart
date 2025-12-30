8885179ab4552476830a17712a2bfff964cb4077
Dirgaaa <dirgayuditama6@gmail.com>
2025-12-30 20:24:16 +0700

    feat: update fields in weather model

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -2,18 +2,25 @@ class Weather {
   final String cityName;
   final double temperature;
   final String mainCondition;
+  final bool isDay;
 
   Weather({
     required this.cityName,
     required this.temperature,
     required this.mainCondition,
+    required this.isDay,
   });
 
   factory Weather.fromJson(Map<String, dynamic> json) {
+    final dt = json['dt'];
+    final sunrise = json['sys']['sunrise'];
+    final sunset = json['sys']['sunset'];
+
     return Weather(
       cityName: json['name'],
       temperature: json['main']['temp'].toDouble(),
       mainCondition: json['weather'][0]['main'],
+      isDay: dt >= sunrise && dt < sunset,
     );
   }
 }
